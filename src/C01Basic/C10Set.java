2960cc5116f8dcceda1da6893141186006695600
HanSungMin99 <sungmini9876@gmail.com>
2024-12-10 14:00:34 +0900

    12/10

--- src/C01Basic/C10Set.java
+++ src/C01Basic/C10Set.java
@@ -22,8 +22,39 @@ public class C10Set {
 //            System.out.println(a);
 //        }
 
-        //프로그래머스 - 폰켓몬
+//        //프로그래머스 - 폰켓몬
 
+//        //LinkedHashSet
+//        Set<Integer> linkedSet = new LinkedHashSet<>();
+//        linkedSet.add(1);
+//        linkedSet.add(1);
+//        linkedSet.add(0);
+//        linkedSet.add(3);
+//        linkedSet.add(1);
+//        System.out.println(linkedSet); //중복제거되고, 입력값 순서대로 출력됨
+//
+//        //TreeSet
+//        Set<Integer> treeSet = new TreeSet<>(); //HashSet보다 성능이 떨어짐: 정렬을 하므로
+//        treeSet.add(1);
+//        treeSet.add(1);
+//        treeSet.add(0);
+//        treeSet.add(3);
+//        treeSet.add(1);
+//        System.out.println(treeSet);//중복제거되고, 정렬되어 출력된다.
+
+//        //집합관련 함수: 교집합(retailAll), 합집합(addAll), 차집합(removeAll)
+//        Set<String> set1 = new HashSet<>();
+//        set1.add("java");
+//        set1.add("python");
+//        set1.add("javascript");
+//        Set<String> set2 = new HashSet<>();
+//        set2.add("java");
+//        set2.add("html");
+//        set2.add("css");
+//        set1.retainAll(set2);
+//        set1.addAll(set2);
+//        set1.removeAll(set2);
+//        System.out.println(set1); //교집합, 합집합, 차집합을 하면 이미 set1값이 바뀌어버리기 때문에 주석처리하고 실행해보아야 함
 
 
     }
---
+++ src/C01Basic/C11QueStackDeque.java
@@ -0,0 +1,24 @@
+package C01Basic;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class C11QueStackDeque {
+    public static void main(String[] args) {
+//        //Queue는 뒤에서 데이터를 넣고 앞에서 데이터를 빼는 것이 가장 최적화 되어 있다.
+//        //Queue 인터페이스를 LinkedList가 구현한 방식을 가장 많이 사용
+//        Queue<Integer> myQueue = new LinkedList<>();
+//        //LinkedList는 List와 Queue 기능을 모두 구현 가능하지만, 왼쪽에 Queue라는 인터페이스를 선언하면서 LinkedList의 사용메서드를 제한함.
+//        //중간에 값을 삽입하는 것은 성능이 저하된다.
+//        myQueue.add(10);
+//        myQueue.add(20);
+//        myQueue.add(30);
+//        //poll을 사용하면 myQueue에 데이터를 삭제하면서 가장 앞의 데이터를 반환한다
+//        System.out.println(myQueue.poll()); //poll은 무조건 맨 앞에 있는 것을 가져옴
+//        System.out.println(myQueue);
+//        //myQueue의 데이터를 삭제하지는 않고, 가장 앞의 데이터를 반환
+//        System.out.println(myQueue.peek());
+//        System.out.println(myQueue);
+
+     }
+}
