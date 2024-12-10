95fff103070a3161daf7375d37e27260af40cb8c
HanSungMin99 <sungmini9876@gmail.com>
2024-12-10 12:07:55 +0900

    12/10

--- src/C01Basic/C10Set.java
+++ src/C01Basic/C10Set.java
@@ -4,23 +4,27 @@ import java.util.*;
 
 public class C10Set {
     public static void main(String[] args) {
-        Set<String> mySet = new HashSet<>();
-        mySet.add("h");
-        mySet.add("h");
-        mySet.add("e");
-        mySet.add("l");
-        mySet.add("l");
-        mySet.add("o");
-        mySet.add("a");
-        mySet.add("b");
-        mySet.add("c");
-        mySet.add("d");
-        System.out.println(mySet);
-        //순서보장 안되고, 중복제거된다.
-        //출력시 index로 접근 불가하다.
-        for(String a : mySet){
-            System.out.println(a);
-        }
+//        Set<String> mySet = new HashSet<>();
+//        mySet.add("h");
+//        mySet.add("h");
+//        mySet.add("e");
+//        mySet.add("l");
+//        mySet.add("l");
+//        mySet.add("o");
+//        mySet.add("a");
+//        mySet.add("b");
+//        mySet.add("c");
+//        mySet.add("d");
+//        System.out.println(mySet);
+//        //순서보장 안되고, 중복제거된다.
+//        //출력시 index로 접근 불가하다.
+//        for(String a : mySet){
+//            System.out.println(a);
+//        }
+
+        //프로그래머스 - 폰켓몬
+
+
 
     }
 }
