c1a0bc04ebf0486afa2b415dfc07eacffaf9722f
HanSungMin99 <sungmini9876@gmail.com>
2024-12-22 17:13:09 +0900

    12/20 오후 강의

--- src/C07ExceptionFileParsing/C0704HttpJsonParsing.java
+++ src/C07ExceptionFileParsing/C0704HttpJsonParsing.java
@@ -1,5 +1,8 @@
 package C07ExceptionFileParsing;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
